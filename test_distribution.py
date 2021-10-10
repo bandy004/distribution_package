9a0bd9e670b443f9150d25d0a67f0e1a5af5304b
bandy004 <banerjee.debdeep@yahoo.com>
2021-10-10 12:26:36 +1100

    added numpy mean and std to test distribution calculations

--- test_distribution.py
+++ test_distribution.py
@@ -1,13 +1,14 @@
+import numpy as np
 from distribution import Distribution
 
 
 def test_mean():
     distribution = Distribution()
     distribution.data = [2, 4, 5, 6, 7, 8, 19, 20, 32, 2, 3, 7, 8]
-    assert(distribution.mean == 0.0)
+    assert(distribution.mean == np.mean(distribution.data))
 
 
 def test_stddev():
     distribution = Distribution()
     distribution.data = [2, 4, 5, 6, 7, 8, 19, 20, 32, 2, 3, 7, 8]
-    assert(distribution.stddev == 0.0)
+    assert(distribution.stddev == np.std(distribution.data))
