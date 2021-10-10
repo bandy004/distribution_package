ff0fb1fc5036cbb8ed01658d0e83c3c4bc5a0986
bandy004 <banerjee.debdeep@yahoo.com>
2021-10-10 12:19:11 +1100

    added test methods for mean and std for Distribution and added code for Distribution class

--- distribution.py
+++ distribution.py
@@ -1,1 +1,23 @@
-"" 
+class Distribution():
+
+    def __init__(self) -> None:
+        ''' This class represents a probability distribution.
+
+        Attributes:
+        data : Array that hold data
+        mean : Mean of the distribution
+        stddev: Standard deviation of the distribution
+
+        Methods:
+        calc_mean : calculate mean from the data
+        calc_stddev: calculate standard deviation from the data 
+        '''
+        self.data = []
+        self.mean = 0.0
+        self.stddev = 0.0
+
+    def calc_mean(self):
+        return self.mean
+
+    def calc_stddev(self):
+        return self.stddev
--- test_distribution.py
+++ test_distribution.py
@@ -1,1 +1,13 @@
-"" 
+from distribution import Distribution
+
+
+def test_mean():
+    distribution = Distribution()
+    distribution.data = [2, 4, 5, 6, 7, 8, 19, 20, 32, 2, 3, 7, 8]
+    assert(distribution.mean == 0.0)
+
+
+def test_stddev():
+    distribution = Distribution()
+    distribution.data = [2, 4, 5, 6, 7, 8, 19, 20, 32, 2, 3, 7, 8]
+    assert(distribution.stddev == 0.0)
